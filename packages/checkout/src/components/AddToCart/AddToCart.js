9edeefaea0836e22c25896e5fdf13c93b1f4e82d
João Paulo Barbosa <joao.barbosa@rdstation.com>
2022-06-11 21:31:30 -0300

    feat(checkout): add some style to components

--- packages/checkout/src/components/AddToCart/AddToCart.js
+++ packages/checkout/src/components/AddToCart/AddToCart.js
@@ -1,9 +1,14 @@
 import { useCallback } from "react"
 import { useCart } from "../Cart/useCart"
+import styles from "./AddToCart.module.css"
 
 export function AddToCart({ children, item }) {
   const { actions } = useCart()
   const handleClick = useCallback(() => actions.add(item), [actions, item])
 
-  return <button onClick={handleClick}>{children}</button>
+  return (
+    <button className={styles.AddToCart} onClick={handleClick}>
+      {children}
+    </button>
+  )
 }
